4552f3f4f1f9afdd2f8a8286180e955d16e60c99
Monik Raj Behera <bmonikraj@gmail.com>
2017-09-03 19:32:09 +0530

    Cropped Face from full face

--- faceDetection.py
+++ faceDetection.py
@@ -3,9 +3,11 @@ def faceDetectedMat(imagePath):
     import cv2
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     img = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
+    if len(img.shape)==3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(img, 1.3, 5)
+    roi_img = []
     for (x,y,w,h) in face:
-        img = cv2.rectangle(img, (x,y), (x+w, y+h), (210,170,20), 5)
-        roi_img = img[y:y+h, x:x+w]
-    return img
+        roi_img.append(img[y:y+h, x:x+w])
+    return roi_img
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import faceDetection
 import cv2
+from matplotlib import pyplot as plt
 
-cv2.namedWindow("show", cv2.WINDOW_NORMAL)
-cv2.imshow('show',faceDetection.faceDetectedMat("c.jpg"))
+
+detectedFaces = faceDetection.faceDetectedMat("b.jpg")
+for i in range(0, len(detectedFaces)):
+    cv2.namedWindow("Face "+str(i+1), cv2.WINDOW_NORMAL)
+    cv2.imshow("Face "+str(i+1),detectedFaces[i])
+
