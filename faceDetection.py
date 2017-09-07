d83c7bb967b1c1913c277ed2660dbdce25f8a534
Monik Raj Behera <bmonikraj@gmail.com>
2017-09-08 02:22:59 +0530

    LBP detector from OpenCV implemented

--- faceDetection.py
+++ faceDetection.py
@@ -1,3 +1,5 @@
+#This code function returns face with landmarks list and landmarks being pointed out with circles
+'''
 def faceDetectedMat(imagePath,HAAR_DETECTOR_PATH, PREDICTOR_PATH):
     import numpy as np
     import cv2
@@ -15,4 +17,23 @@ def faceDetectedMat(imagePath,HAAR_DETECTOR_PATH, PREDICTOR_PATH):
             cv2.putText(img, str(i), (landmarks[i,0], landmarks[i,1]),fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,fontScale=0.4,color=(0, 0, 0))
             cv2.circle(img, (landmarks[i,0], landmarks[i,1]), 5, color=(255,255,255))
     return img,np.array(landmarks).tolist()
-
+'''
+def faceDetectedMat(imagePath,HAAR_DETECTOR_PATH, PREDICTOR_PATH):
+    import numpy as np
+    import cv2
+    import dlib
+    predictor = dlib.shape_predictor(PREDICTOR_PATH)
+    face_cascade = cv2.CascadeClassifier(HAAR_DETECTOR_PATH)
+    img = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
+    if len(img.shape)==3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(img, 1.3, 5)
+    if type(face)==np.ndarray:
+        x = face[0][0]
+        y = face[0][1]
+        w = face[0][2]
+        h = face[0][3]
+        rect = img[y:y+h, x:x+w]
+        return rect
+    else:
+        return None
--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import faceDetection
 import cv2
+import os
+import numpy as np
 
-TEST_IMAGE_PATH = "1.pgm"
+TEST_IMAGE_PATH = "2.pgm"
+DATASET_PATH = "dbc"
 SHAPE_PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
 HAAR_CASCADE_PATH = "haarcascade_frontalface_default.xml"
 
-detectedFace, landmarkPoints = faceDetection.faceDetectedMat(TEST_IMAGE_PATH, HAAR_CASCADE_PATH, SHAPE_PREDICTOR_PATH)
+#snippet for file testing
+'''
+detectedFace = faceDetection.faceDetectedMat(TEST_IMAGE_PATH, HAAR_CASCADE_PATH, SHAPE_PREDICTOR_PATH)
 cv2.namedWindow("Face Landmarks Detection", cv2.WINDOW_NORMAL)
 cv2.imshow("Face Landmarks Detection",detectedFace)
+'''
+
+
+recognizer = cv2.face.createLBPHFaceRecognizer()
+
+def getTrainData(path):
+    image_files = []
+    labels = []
+    main_dir = path
+    for person in os.listdir(main_dir):
+        foldername = person
+        person_dir = os.path.join(main_dir,foldername)
+        for file in os.listdir(person_dir):
+            file_path = os.path.join(person_dir,file)
+            print(file_path)
+            x = faceDetection.faceDetectedMat(file_path, HAAR_CASCADE_PATH, SHAPE_PREDICTOR_PATH)
+            if x!=None:
+                print(x)
+                image_files.append(x)
+                labels.append(foldername)           
+            
+    return image_files, labels
+
+
+images, labels = getTrainData(DATASET_PATH)
+recognizer.train(images, np.array(labels))
+
+name_predicted, cnf = recognizer.predict(faceDetection.faceDetectedMat(TEST_IMAGE_PATH, HAAR_CASCADE_PATH, SHAPE_PREDICTOR_PATH))
+
+print("The predicted Name : " + str(name_predicted)+" with confidence of "+str(cnf))
 
